5c69b305f1242c036d6a8bb123f8e5de3f0f2b7d
Gabriel Lopez <gabriellopez.dev@gmail.com>
2022-07-12 22:39:58 -0400

    [add] configurar layouts y vista principal

---
+++ app/Http/Livewire/CustomerSupport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CustomerSupport extends Component
+{
+    public function render()
+    {
+        return view('livewire.customer-support');
+    }
+}
---
+++ resources/views/livewire/customer-support.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="max-w-7xl mx-auto rounded px-4 sm:px-6 lg:px-8 py-6 grid grid-cols-2 grid-rows-1 bg-white mt-10">
+
+        <h1>Crear ticket</h1>
+
+    </div>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 grid grid-cols-2 grid-rows-1 bg-white mt-6">
+
+        <h1>Mostrar colas</h1>
+
+    </div>
+</div>
